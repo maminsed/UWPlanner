8c7d429e3f3aa0e4294fb53f0d1f37d9dafb9830
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-04-10 00:59:45 -0400

    Initialized Landing

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
+import Landing from "@/components/Landing";
 import Navbar from "@/components/Navabar";
 
 export default function Home() {
   return (
     <section className="overflow-x-hidden">
-      <Navbar />
+      <Navbar />  
+      <Landing />
     </section>
   );
 }
---
+++ frontend/src/components/Landing.tsx
@@ -0,0 +1,9 @@
+
+export default function Landing() {
+
+    return (
+        <section>
+            Hello
+        </section>
+    )
+}

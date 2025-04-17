c00490ea60d1cbe62928c384760691387458c3b9
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-04-16 23:25:14 -0400

    Set up login page

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import Login from "@/components/Login";
+
+export default function Landing() {
+  return (
+    <section className="overflow-x-hidden flex justify-center items-center h-dvh w-dvw">
+        <Login />
+    </section>
+  );
+}

7bd870ec566c8c456f770ae81339e3705eb275d7
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-04-19 13:04:51 -0400

    Add password visibility toggle

--- frontend/src/components/AuthForm.tsx
+++ frontend/src/components/AuthForm.tsx
@@ -1,18 +1,39 @@
+'use client';
 import Link from "next/link";
+import { useState } from "react";
 import { FaGoogle } from "react-icons/fa";
 import { FaApple } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import { IoMdEye,IoMdEyeOff } from "react-icons/io";
 
 export default function SignIn({mode}:{mode:"Sign In"|"Sign Up"}) {
+    const [visiblePass, setVisiblePass] = useState("password");
+
+    function reverseVisibility() {
+        setVisiblePass(visiblePass == "password" ? "text" : "password");
+    }
+
+
     return (
         <div className="max-xs:mt-25 max-w-[96vw] w-[270px] flex flex-col pt-8 rounded-xl bg-white shadow-[0_2px_49.9px_0_#222E3033] overflow-x-hidden">
             <h3 className="w-full text-center text-3xl font-bold z-2">{mode}</h3>
             <div className="w-full h-full bg-dark-green p-6 pt-8 pb-8 mt-6 rounded-xl rounded-t-3xl text-light-green drop-shadow-[0_-2px_19.1px_rgba(47,55,56,0.25)]">
                 <form className="flex flex-col">
                     <input name="Email" required={true} className="mb-2 border-2 border-light-green rounded-xl pl-3 py-2 text-sm focus:outline-none focus:shadow-[0px_0px_5px_0px_#ECFDF5] transition-all" placeholder="Email" type="email"/>
-                    <input name="password" required={true} className="border-2 border-light-green rounded-xl pl-3 py-2 text-sm focus:outline-none focus:shadow-[0px_0px_5px_0px_#ECFDF5] transition-all mb-2" placeholder="Password" type="password"/>
-                    {mode == "Sign In" ? <a className="mr-1.5 cursor-pointer text-xs ml-auto underline">Forgot Password?</a> : 
-                    <input name="password" required={true} className="border-2 border-light-green rounded-xl pl-3 py-2 text-sm focus:outline-none focus:shadow-[0px_0px_5px_0px_#ECFDF5] transition-all mb-2" placeholder="Confirm Password" type="password"/>}
+                    <div className="flex relative mb-2">
+                        <input name="password" required={true} className="border-2 border-light-green rounded-xl pl-3 pr-6 py-2 text-sm focus:outline-none focus:shadow-[0px_0px_5px_0px_#ECFDF5] transition-all w-full" placeholder="Password" type={visiblePass} />
+                        {visiblePass == "password" ?
+                            <IoMdEyeOff className="absolute right-2 top-[50%] translate-y-[-50%] cursor-pointer" onClick={reverseVisibility} style={{height:"18px", width:"auto"}}/> : 
+                            <IoMdEye className="absolute right-2 top-[50%] translate-y-[-50%] cursor-pointer" onClick={reverseVisibility} style={{height:"18px", width:"auto"}}/>}
+                    </div>
+                    {mode == "Sign In" ? 
+                        <a className="mr-1.5 cursor-pointer text-xs ml-auto underline">Forgot Password?</a> : 
+                        <div className="flex relative mb-2">
+                            <input name="password" required={true} className="border-2 border-light-green rounded-xl pl-3 pr-6 py-2 text-sm focus:outline-none focus:shadow-[0px_0px_5px_0px_#ECFDF5] transition-all w-full" placeholder="Password" type={visiblePass} />
+                            {visiblePass == "password" ?
+                                <IoMdEyeOff className="absolute right-2 top-[50%] translate-y-[-50%] cursor-pointer" onClick={reverseVisibility} style={{height:"18px", width:"auto"}}/> : 
+                                <IoMdEye className="absolute right-2 top-[50%] translate-y-[-50%] cursor-pointer" onClick={reverseVisibility} style={{height:"18px", width:"auto"}}/>}
+                        </div>}
                     <button type="submit" className="mt-16 cursor-pointer rounded-3xl bg-light-green text-dark-green py-2 hover:bg-[#00b03e] hover:text-light-green transition-all duration-500 active:bg-green-600">{mode}</button>
                 </form>
 

18db62cc77ce7d57fd46b153edc06f76f746c03a
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-04-19 00:00:07 -0400

    Added Navbar to signup and singin

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,8 +1,10 @@
 import AuthForm from "@/components/AuthForm";
+import Navbar from "@/components/Navabar";
 
 export default function Landing() {
   return (
     <section className="overflow-x-hidden flex justify-center items-center h-dvh w-dvw">
+        <Navbar />
         <AuthForm mode="Sign In"/>
     </section>
   );

366547d8ba4d9a4df81bdda5183fc41be9bffee5
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-04-25 10:32:10 -0400

    Added function header and types for print_table

--- backend/Schema/neon.py
+++ backend/Schema/neon.py
@@ -2,9 +2,18 @@ from sqlalchemy import create_engine,text
 from dotenv import load_dotenv
 import os, pandas as pd
 
-def print_table(table_name, cols=["*"]):
+def print_table(table_name:str, cols:list[str]=["*"]):
     """
         prints the table at table_name with cols. mainly a utility function for development
+
+        Args:
+            table_name (str):
+                name of the table
+            cols (list(str), optional):
+                the columns you wnat. Defaults to all.
+        
+        Returns:
+            None
     """
     load_dotenv()
     engine = create_engine(os.getenv('DATABASE_URL'))

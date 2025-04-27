0048b4b7f5ee2943532841efd8957eda3b049a92
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-04-27 11:23:21 -0400

    Updated insert

--- backend/Schema/insert.py
+++ backend/Schema/insert.py
@@ -1,11 +1,8 @@
-from sqlalchemy import create_engine, insert, Table, MetaData
-from sqlalchemy.sql import func
-from dotenv import load_dotenv
-import os
+from db import Users, db, app
+from datetime import datetime
 
-if __name__ == '__main__':
-    load_dotenv()
-    engine = create_engine(os.getenv('DATABASE_URL'))
-    table = Table('user_table', MetaData(), autoload_with=engine)
-    with engine.begin() as conn:
-        conn.execute(insert(table).values(user_name='susy', pass_hash='baka', login_method=1, created_at=func.now(), last_updated_at=func.now()))
+if __name__ == "__main__": 
+    with app.app_context():
+        u = Users(user_name="fat", pass_hash="obesityNig", login_method=12, created_at=datetime.now(), last_updated_at=datetime.now())
+        db.session.add(u)
+        db.session.commit()

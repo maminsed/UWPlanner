fc3c598469f27353828d9037c7b1bc459d94a860
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-04-28 23:22:15 -0400

    Fixed types and moved app to it's correct location

--- backend/Schema/db.py
+++ backend/Schema/db.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Enum
 from datetime import datetime
 from dotenv import load_dotenv
+from enum import Enum as Pyenum
 import os
 load_dotenv()
 
@@ -10,18 +12,20 @@ class Base(DeclarativeBase):
     pass
 
 db = SQLAlchemy(model_class=Base)
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-db.init_app(app)
+
+
+class LoginMethod(Pyenum):
+    email  = "email"
+    google = "google"
+    apple  = "apple"
+    github = "github"
 
 class Users(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     user_name: Mapped[str] = mapped_column(db.String(50), nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(db.String(80), nullable=False, unique=True)
     pass_hash: Mapped[str] = mapped_column(db.String(80), nullable=False)
-    login_method: Mapped[int] = mapped_column(db.SmallInteger, nullable=False)
+    login_method: Mapped[LoginMethod] = mapped_column(Enum(LoginMethod, name="login_method", native_enum=True, create_type=False), nullable=False)
     created_at: Mapped[datetime] = mapped_column(db.TIMESTAMP, nullable=False)
     last_updated_at: Mapped[datetime] = mapped_column(db.TIMESTAMP, nullable=False)
     academic_info_id: Mapped[int | None] = mapped_column(db.Integer)
-
-with app.app_context():
-    db.create_all()

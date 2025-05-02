777fd80136458f1e977fe4b4d068e6f3deb94953
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-05-01 22:40:22 -0400

    Added jwt to database

--- backend/Schema/db.py
+++ backend/Schema/db.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Enum
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import Enum, ForeignKey
 from datetime import datetime
 from dotenv import load_dotenv
 from enum import Enum as Pyenum
@@ -29,4 +29,13 @@ class Users(db.Model):
     login_method: Mapped[LoginMethod] = mapped_column(Enum(LoginMethod, name="login_method", native_enum=True, create_type=False), nullable=False)
     created_at: Mapped[datetime] = mapped_column(db.DateTime(timezone=True), nullable=False, server_default=db.func.now())
     last_updated_at: Mapped[datetime] = mapped_column(db.DateTime(timezone=True), nullable=False, server_default=db.func.now(), onupdate=db.func.now())
-    academic_info_id: Mapped[int | None] = mapped_column(db.Integer)
+    refresh_tokens: Mapped[list["JwtToken"]] = relationship("JwtToken", back_populates="user", cascade="all, delete-orphan, save-update")
+
+
+    
+class JwtToken(db.Model):
+    jwt_token_id: Mapped[int] = mapped_column(primary_key=True)
+    refresh_Token_string: Mapped[str] = mapped_column(db.String(), nullable=False)
+    user_id: Mapped[int] = mapped_column(db.Integer, ForeignKey('users.id'), nullable=False)
+
+    user: Mapped["Users"] = relationship("Users", back_populates="refresh_tokens")

713a1fe3a1baccfc0d488881e7e7ae99a5cb6314
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-05-10 19:05:26 -0400

    Connected test route to backend

--- frontend/src/app/test/page.tsx
+++ frontend/src/app/test/page.tsx
@@ -1,13 +1,34 @@
 'use client'
-import { useContext } from "react";
-import { AuthContext } from "../AuthProvider"
+import { useEffect, useState } from "react";
+import { api } from "@/lib/useApi";
 
 export default function Test() {
-    const { access } = useContext(AuthContext);
+    const [message, setMessage] = useState<string>()
+    const backend = api();
+    useEffect(()=>{
+        async function initial_handle() {
+            try {
+                const res = await backend(`${process.env.NEXT_PUBLIC_API_URL}/test/`, {
+                method: "GET",})
+
+                if (!res?.ok) {
+                    throw new Error("Error in Backend")
+                }
+                const response = await res.json()
+                setMessage(response.message)
+            } catch (err) {
+                setMessage("Error in Response")
+            }
+            
+        }
+
+        initial_handle()
+
+    }, [])
     
     return (
         <div className="bg-amber-400 text-green-300 text-5xl">
-            Meow Meow Bitch!
+            {message}
         </div>
     )
 }

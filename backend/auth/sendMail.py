50b2af0c2de331d7024d98762488f1fbfe459a84
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-05-27 22:30:08 -0400

    updated sendMail to acctually send mail

--- backend/auth/sendMail.py
+++ backend/auth/sendMail.py
@@ -1,32 +1,24 @@
-import os
-import smtplib
-import ssl
+import random
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
-port = 465
-password = os.getenv("MAIL_PASSWORD")
-print(password)
+from ..google_api.gmail_api import gmail_send_message
+from ..Schema import Users
 
 
-def send_email():
-    context = ssl.create_default_context()
-    message = """\
-    Subject: Hi there
+def send_verification_mail(user:Users)->None:
+    """Sends a verification email to user and saves the code in database.
 
+    Requires:
+        user (Users):
+            The user which is being sent to.
+    
+    """
+    code=random.randint(100000, 999999)
+    body=f"""
+Hi,
 
-    This message is sent from Python. TOUCH MEEE."""
+Your verification code is: [{code}]
+This verification code will expire in 30 minutes.
 
-    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-        server.login("noreply.uwplanner@gmail.com", password)
-        # SEND EMAIL
-        server.sendmail(
-            "noreply.uwplanner@gmail.com", "m.aminsedaghat84@gmail.com", message
-        )
-    print("Hello")
-
-
-if __name__ == "__main__":
-    send_email()
+This e-mail was sent from a notification-only address that cannot accept incoming e-mails. Please do not reply to this message. 
+    """
+    gmail_send_message(to=user.email, body=body, subject="Verification Code For UWPlanner")

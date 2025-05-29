041318451c869e82e1075121dff2dd479419db22
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-05-29 14:28:14 -0400

    styled verify

--- frontend/src/app/verify/page.tsx
+++ frontend/src/app/verify/page.tsx
@@ -25,10 +25,10 @@ export default function Verify() {
     return (
         <main className="relative h-dvh overflow-x-hidden flex justify-center items-center">
             <Image className="absolute object-cover object-center left-[-2px] right-[-2px] w-[calc(100vw+15rem)]  md:w-[calc(100vw+4px)] max-w-none" src="/backgroundLogin.svg" alt="background2" width={1437} height={496}/>
-            <section className="z-2 bg-dark-green text-light-green flex flex-col items-center">
-                <h3>Two-Factor Verification</h3>
-                <p>Enter the code sent to the email: </p>
-                <form>
+            <section className="z-2 bg-dark-green text-light-green flex flex-col items-center py-6 px-3 rounded-lg">
+                <h3 className="text-2xl font-bold leading-[98%] mb-1">Two-Factor <br/> Verification</h3>
+                <p className="text-sm mb-6">Enter the code sent to the email: <br/> <span>username.gmail.com</span></p>
+                <form className="flex flex-col items-center">
                     <div className="relative">
                         <div className="flex gap-2 justify-center">
                             {code.map((item, index)=> (
@@ -53,8 +53,8 @@ export default function Verify() {
                             className="absolute font-mono inset-0 w-full h-full text-amber-800 pl-[0.92rem] tracking-[1.61rem] text-2xl font-semibold opacity-0"
                             />
                     </div>
-                    <p>didn't recieve a code? <button>resend</button></p>
-                    <button type="submit">Verify</button>
+                    <p className="text-sm text-center mt-1 mb-5">didn't recieve a code? <button type="button" className="text-cyan-400 underline cursor-pointer">resend</button></p>
+                    <button type="submit" className="bg-light-green text-dark-green px-8 py-1 rounded-md font-semibold cursor-pointer active:inset-shadow-sm active:inset-shadow-dark-green transition duration-200 ease-in">Verify</button>
                 </form>
             </section>
         </main>

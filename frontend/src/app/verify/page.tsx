145bb6ffe933917568b2e4c07fc8e52d8b48ba71
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-06-01 13:10:04 -0400

    Implemented the sending of the verification code

--- frontend/src/app/verify/page.tsx
+++ frontend/src/app/verify/page.tsx
@@ -17,17 +17,40 @@ export default function Verify() {
     const [code, setCode] = useState<string[]>(['','','','','',''])
     //                                                 start, end
     const [last, setLast] = useState<[number, number]>([0,0])
+    const [email, setEmail] = useState<string>('')
     const { handleSubmit, register, setError, formState: {errors} } = useForm<FormFields>({ resolver: zodResolver(Schema)})
     const {username} = useAuth()
     const router = useRouter()
 
     useEffect(()=>{
-        console.log(username)
         if (!username) {
             console.log("please logIn First")
             router.push("/login")
+            return
         }
+        sendVerification()
+
     }, [])
+
+    async function sendVerification() {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/refresh_veri`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            credentials: "include",
+            body: JSON.stringify({
+                "username": username,
+            })
+        })
+
+        const res = await response.json().catch(()=>{})
+        if (!response.ok) {
+            if (res.action == "main_page") {
+                router.push("/test")
+            }
+            return 
+        }
+        setEmail(res.email);
+    }
 
     const onSubmit: SubmitHandler<FormFields> = async (data) => {
         const finalCode = code.join("").trim()
@@ -46,7 +69,6 @@ export default function Verify() {
                     "code": finalCode,
                 })
             })
-            console.log(response)
             const res = await response.json().catch(()=>{})
             if (!response.ok) {
                 setError("code", {
@@ -83,7 +105,7 @@ export default function Verify() {
             <Image className="absolute object-cover object-center left-[-2px] right-[-2px] w-[calc(100vw+15rem)]  md:w-[calc(100vw+4px)] max-w-none" src="/backgroundLogin.svg" alt="background2" width={1437} height={496}/>
             <section className="z-2 bg-dark-green text-light-green flex flex-col items-center py-6 px-3 rounded-lg">
                 <h3 className="text-2xl font-bold leading-[98%] mb-1">Two-Factor <br/> Verification</h3>
-                <p className="text-sm mb-6">Enter the code sent to the email: <br/> <span>username.gmail.com</span></p>
+                <p className="text-sm mb-6 text-center">Enter the code sent to the email: <br/> <span>{email}</span></p>
                 <form className="flex flex-col items-center" onSubmit={handleSubmit(onSubmit)}>
                     <div className="relative">
                         <div className="flex gap-2 justify-center">
@@ -110,7 +132,7 @@ export default function Verify() {
                             className="absolute font-mono inset-0 w-full h-full text-amber-800 pl-[0.92rem] tracking-[1.61rem] text-2xl font-semibold opacity-0"
                             />
                     </div>
-                    <p className="text-sm text-center mt-1 mb-5">didn't recieve a code? <button type="button" className="text-cyan-400 underline cursor-pointer">resend</button></p>
+                    <p className="text-sm text-center mt-1 mb-5">didn't recieve a code? <button type="button" onClick={sendVerification} className="text-cyan-400 underline cursor-pointer">resend</button></p>
                     {errors?.code && <p className="text-sm text-red-400 mb-1">{errors.code.message}</p>}
                     <button type="submit" className="bg-light-green text-dark-green px-8 py-1 rounded-md font-semibold cursor-pointer active:inset-shadow-sm active:inset-shadow-dark-green transition duration-200 ease-in">Verify</button>
                 </form>

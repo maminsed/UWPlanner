6f431a4d33a60bfff6927b77eaa0ddbaa3fb5c33
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-06-01 12:56:14 -0400

    Enhance verification process: implement form submission, error handling, and user feedback for code input.

--- frontend/src/app/verify/page.tsx
+++ frontend/src/app/verify/page.tsx
@@ -1,15 +1,71 @@
 'use client'
 import Image from "next/image"
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useAuth } from "../AuthProvider";
+import { useRouter } from "next/navigation";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const Schema = z.object({
+    code: z.string()
+})
+
+type FormFields = z.infer<typeof Schema>;
+
 export default function Verify() {
     const [code, setCode] = useState<string[]>(['','','','','',''])
     //                                                 start, end
     const [last, setLast] = useState<[number, number]>([0,0])
+    const { handleSubmit, register, setError, formState: {errors} } = useForm<FormFields>({ resolver: zodResolver(Schema)})
+    const {username} = useAuth()
+    const router = useRouter()
+
+    useEffect(()=>{
+        console.log(username)
+        if (!username) {
+            console.log("please logIn First")
+            router.push("/login")
+        }
+    }, [])
+
+    const onSubmit: SubmitHandler<FormFields> = async (data) => {
+        const finalCode = code.join("").trim()
+        console.log(finalCode)
+        if (finalCode.length < 6) {
+            setError("code", {"message": "Please Enter 6 characters"})
+            return 
+        }
+        try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/confirm_veri`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                credentials: "include",
+                body: JSON.stringify({
+                    "username": username,
+                    "code": finalCode,
+                })
+            })
+            console.log(response)
+            const res = await response.json().catch(()=>{})
+            if (!response.ok) {
+                setError("code", {
+                    "message": res.message || "error"
+                })
+            } else {
+                console.log("Congrats: "+res.message)
+            }   
+        } catch (err) {
+            setError("code", {
+                "message": err instanceof Error ? err.message : "Error - Please Try again"
+            })
+        }
+    }
 
     function handleChange(e:React.ChangeEvent<HTMLInputElement>) {
         const data = e.target.value.split('');
         const res:string[] = []
-        data.map((item)=>res.push(item))
+        data.map((item)=>res.push(item));
         let i = res.length;
         while (i < 6) {
             res.push('')
@@ -28,7 +84,7 @@ export default function Verify() {
             <section className="z-2 bg-dark-green text-light-green flex flex-col items-center py-6 px-3 rounded-lg">
                 <h3 className="text-2xl font-bold leading-[98%] mb-1">Two-Factor <br/> Verification</h3>
                 <p className="text-sm mb-6">Enter the code sent to the email: <br/> <span>username.gmail.com</span></p>
-                <form className="flex flex-col items-center">
+                <form className="flex flex-col items-center" onSubmit={handleSubmit(onSubmit)}>
                     <div className="relative">
                         <div className="flex gap-2 justify-center">
                             {code.map((item, index)=> (
@@ -43,9 +99,10 @@ export default function Verify() {
                             ))}
                         </div>
                         <input 
-                            type="tel" 
+                            type="tel"
+                            {...register("code")}
                             inputMode="numeric" 
-                            autoComplete="one-time-code" 
+                            autoComplete="one-time-code"
                             maxLength={6}
                             onChange={handleChange}
                             onKeyUp={handleKeyUp}
@@ -54,11 +111,10 @@ export default function Verify() {
                             />
                     </div>
                     <p className="text-sm text-center mt-1 mb-5">didn't recieve a code? <button type="button" className="text-cyan-400 underline cursor-pointer">resend</button></p>
+                    {errors?.code && <p className="text-sm text-red-400 mb-1">{errors.code.message}</p>}
                     <button type="submit" className="bg-light-green text-dark-green px-8 py-1 rounded-md font-semibold cursor-pointer active:inset-shadow-sm active:inset-shadow-dark-green transition duration-200 ease-in">Verify</button>
                 </form>
             </section>
         </main>
     )
 }
-
-//

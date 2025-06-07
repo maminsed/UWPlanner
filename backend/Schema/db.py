4ac7c0fcd0395ec92974e9938156b259757fbf32
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-06-06 20:51:58 -0400

    created the major database and added relationships

--- backend/Schema/db.py
+++ backend/Schema/db.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from enum import Enum as Pyenum
+from typing import Optional
 
 from dotenv import load_dotenv
 from flask_migrate import Migrate
@@ -41,6 +42,7 @@ class Users(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(db.String(50), nullable=False, unique=True)
     email: Mapped[str] = mapped_column(db.String(80), unique=True, nullable=False)
+    # Authorization - Authentication Information
     pass_hash: Mapped[str] = mapped_column(db.String(), nullable=False)
     login_method: Mapped[LoginMethod] = mapped_column(
         Enum(LoginMethod, name="login_method", native_enum=True, create_type=False),
@@ -67,6 +69,15 @@ class Users(db.Model):
     refresh_tokens: Mapped[list["JwtToken"]] = relationship(
         "JwtToken", back_populates="user", cascade="all, delete-orphan, save-update"
     )
+    #School related Information
+    major_id: Mapped[Optional[int]] = mapped_column(ForeignKey("major.id"))
+    major: Mapped[Optional["Major"]] = relationship("Major", back_populates="main_major_users", foreign_keys=[major_id])
+
+    second_major_id: Mapped[Optional[int]] = mapped_column(ForeignKey("major.id"))
+    second_major: Mapped[Optional["Major"]] = relationship("Major", back_populates="second_major_users", foreign_keys=[second_major_id])
+
+
+
 
 
 class JwtToken(db.Model):
@@ -80,3 +91,17 @@ class JwtToken(db.Model):
     )
 
     user: Mapped["Users"] = relationship("Users", back_populates="refresh_tokens")
+
+class Major(db.Model):
+    """Database for the majors"""
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[int] = mapped_column(db.String, nullable=False)
+    main_major_users: Mapped[list["Users"]] = relationship("Users", back_populates="major", foreign_keys="[Users.major_id]")
+    second_major_users: Mapped[list["Users"]] = relationship("Users", back_populates="second_major", foreign_keys="Users.second_major_id")
+
+# class Minor(db.Model):
+#     """Database for the minors"""
+
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     users: Mapped[list[Optional["Users"]]] = relationship("Users", back_populates="minors")

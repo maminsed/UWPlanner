971e9cc4d8113e5803a0c38b562606dd8bdbfdb7
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-06-21 20:10:49 -0400

    added DropDown Component

--- frontend/src/app/signUp/info/page.tsx
+++ frontend/src/app/signUp/info/page.tsx
@@ -1,6 +1,10 @@
+'use client';
 import { LuCirclePlus } from "react-icons/lu"
+import DropDown from "@/components/DropDown";
 
 export default function Info() {
+
+    
 
     return (
         <main>
@@ -8,39 +12,7 @@ export default function Info() {
 
             <div className="mx-auto w-fit mt-20 px-6 py-5 rounded-lg bg-[#DAEBE3] shadow-[0px_0px_57.4px_0px_rgba(0,0,0,0.4)]">
                 <h5 className="text-2xl font-medium mb-10 text-center">What's your Major?</h5>
-                <div className="relative">
-                    <select name="major" defaultValue="default" className="w-64 bg-light-green px-1 pr-6 py-1 rounded-md appearance-none focus:outline-none">
-                        <option value="default" disabled={true}>Choose from the list below</option>
-                        <option value="Communication Studies">Communication Studies</option>
-                        <option disabled={true}>Engineering</option>
-                        <option value="Global Business and Digital Arts">Global Business and Digital Arts</option>
-                        <option value="Global Business and Digital Arts">Global Business and Digital Arts</option>
-                        <option value="Global Business and Digital Arts">Global Business and Digital Arts</option>
-                        <option value="Global Business and Digital Arts">Global Business and Digital Arts</option>
-                        <option value="Global Business and Digital Arts">Global Business and Digital Arts</option>
-                        <option value="Global Business and Digital Arts">Global Business and Digital Arts</option>
-                        <option value="Global Business and Digital Arts">Global Business and Digital Arts</option>
-                        <option value="Global Business and Digital Arts">Global Business and Digital Arts</option>
-                        <option value="Global Business and Digital Arts">Global Business and Digital Arts</option>
-                        <option value="Global Business and Digital Arts">Global Business and Digital Arts</option>
-                        <option value="Global Business and Digital Arts">Global Business and Digital Arts</option>
-                        <option value="Global Business and Digital Arts">Global Business and Digital Arts</option>
-                        <option value="Business Administration (Laurier) and Computer Science (Waterloo) Double Degree">Business Administration (Lauri.</option>
-                    </select>
-                    <span className="pointer-events-none absolute inset-y-0 right-1 flex items-center">
-                        <svg
-                            className="w-6 h-6 text-dark-green"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                        >
-                            <path
-                                d="M5.23 7.21a.75.75 0 011.06.02L10 10.94l3.71-3.71a.75.75 
-                                0 011.08 1.04l-4.25 4.25a.75.75 0 01-1.08 
-                                0L5.21 8.27a.75.75 0 01.02-1.06z"
-                            />
-                        </svg>
-                    </span>
-                </div>
+                <DropDown />
                 <button className="mt-4"><LuCirclePlus /></button>
             </div>
 

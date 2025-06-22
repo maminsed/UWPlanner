b28c035b485b422c6c0a30928e70b2c01683e5b0
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-06-21 23:27:52 -0400

    Fixed Issues with HoverEffect

--- frontend/src/components/HoverEffect.tsx
+++ frontend/src/components/HoverEffect.tsx
@@ -1,13 +1,23 @@
 import { HTMLAttributes } from "react"
 
 interface HoverEffectProps extends HTMLAttributes<HTMLSpanElement> {
-    className?: string;
+    outerClass?: string;
+    pClass?: string;
+    hoverClass?: string;
     text: string;
+    maxWidth?: string;
 }
 
-export default function HoverEffect({ className,text, ...props}: HoverEffectProps) {
+export default function HoverEffect({ outerClass="", pClass="", hoverClass="",text, maxWidth="100px", ...props}: HoverEffectProps) {
 
     return (
-        <span className={className} {...props}>{text}</span>
+        <div className={"group relative w-fit "+outerClass} {...props}>
+            <p className={"truncate w-fit "+pClass} style={{maxWidth:maxWidth}}>
+                {text}
+            </p>
+            <div className={"absolute max-h-0 py-0 group-hover:max-h-15 transition-all duration-600 bg-slate-900/70 overflow-y-hidden text-green-100 text-xs px-2 group-hover:py-1 rounded-md top-5 -right-2 z-20 "+hoverClass}>
+                {text}
+            </div>
+        </div>
     )
 }

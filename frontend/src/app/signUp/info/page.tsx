c0aeb02876547acb14985163a251b9ecc8324bc0
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-06-22 21:39:35 -0400

    added list of majors

--- frontend/src/app/signUp/info/page.tsx
+++ frontend/src/app/signUp/info/page.tsx
@@ -1,19 +1,42 @@
 'use client';
-import { LuCirclePlus } from "react-icons/lu"
+import { LuCircleMinus, LuCirclePlus } from "react-icons/lu"
 import DropDown from "@/components/DropDown";
+import { useState } from "react";
 
 export default function Info() {
+    const [nextId, setNextId] = useState<number>(1)
+    const [dropIds, setDropIds] = useState<[number, string][]>([[0,"Choose your option"]])
 
-    
+    function handleAdd() {
+        setNextId(nextId+1)
+        setDropIds((ids)=>[...ids,[nextId, "Choose your option"]])
+    }
 
+    function handleRemove(id:number) {
+        setDropIds((ids)=>ids.filter(x=>x[0] !== id))
+    }
+
+    function handleUpdate(id:number, value:string) {
+        setDropIds((ids)=>ids.map(item=> {
+            if(item[0] == id) {
+                return [id, value]
+            }
+            return item
+        }))
+    }
     return (
         <main>
-            <h2 className="md:mt-15 px-3 mt-8 text-center md:text-2xl text-xl font-semibold">Just a few more questions to know you better</h2>
+            <h2 className="md:mt-15 px-3 mt-5 text-center md:text-2xl text-xl font-semibold">Just a few more questions to know you better</h2>
 
             <div className="mx-auto w-fit mt-20 px-6 py-5 rounded-lg bg-[#DAEBE3] shadow-[0px_0px_57.4px_0px_rgba(0,0,0,0.4)]">
                 <h5 className="text-2xl font-medium mb-10 text-center">What's your Major?</h5>
-                <DropDown />
-                <button className="mt-4"><LuCirclePlus /></button>
+                {dropIds.map(item => (
+                    <div key={item[0]} className="flex items-center gap-2">
+                        <DropDown className="mt-1" selectedValue={item[1]} setSelectedValue={(value)=>handleUpdate(item[0], value)}/>
+                        {dropIds.length != 1 && <LuCircleMinus onClick={()=>handleRemove(item[0])}/>}
+                    </div>
+                ))}
+                <button className="mt-2" onClick={handleAdd}><LuCirclePlus /></button>
             </div>
 
             <div className="h-[50vh] md:h-fit w-dvw fixed left-0 bottom-0 overflow-x-hidden z-[-1]">

06646dd216ffa6b7121061a1a049cc6042a54e55
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-06-23 20:54:35 -0400

    added add_minor

--- backend/Login_actions/action.py
+++ backend/Login_actions/action.py
@@ -2,7 +2,7 @@ from typing import Optional
 from flask import Blueprint, jsonify, make_response, g, request
 from backend.Schema import Major, Minor
 from backend.Auth import verify as verify_jwt
-from ..School_info import enrol_to_major
+from ..School_info import enrol_to_major, enrol_to_minor
 
 from collections import defaultdict
 
@@ -63,3 +63,29 @@ def get_minors() -> tuple[str,int]:
         return jsonify({"data": [[f,res[f]] for f in res.keys()]}), 200
     except Exception as e:
         return jsonify({"message": "error in backend", "error": str(e)}), 500
+    
+@update_info.route("/minors", methods=["POST"])
+def add_minor() -> tuple[str,int]:
+    username = g.username
+    data = request.get_json()
+    minors = data.get("selected")
+    if not username:
+        return jsonify({"message": "Please sign in first"}), 401
+    if not minors or len(minors) == 0:
+        return "", 204
+    
+    #Checking for duplicates:
+    visited = set()
+    for m in minors:
+        if m in visited:
+            return jsonify({"message": "You have duplicate minors."}), 400
+        visited.add(m)
+    
+    
+    for m in visited:
+        status,message = enrol_to_minor(m, username)
+        if status == 500:
+            return jsonify({"message": "error in backend", "error": message}), status
+        if status >= 400 and status <= 500:
+            return jsonify({"message": message}), status
+    return jsonify({"message": "user enroled!"}), 200

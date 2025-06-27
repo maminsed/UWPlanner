eae74cc6ab298f0352d997d373f0c503b4e34dc7
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-06-27 11:41:49 -0400

    improved documentation and adding process

--- backend/School_info/enrolement.py
+++ backend/School_info/enrolement.py
@@ -1,9 +1,21 @@
 from ..Schema import Major, Minor, Sequence, Specialization, Users, db
 
 
-def enrol_to_major(major_name: str, username: str) -> tuple[int, str]:
-    """Function to enrol a student in a major. Not Complete"""
-    major = Major.query.filter_by(name=major_name).first()
+def enrol_to_major(major_id: str, username: str) -> tuple[int, str]:
+    """Function to enrol a student in a major.
+    
+    Requires:
+        - major_name (str):
+            The name of the major
+        - username (str):
+            The student to add.
+    
+    Returns:
+        the status of this + message.
+
+    """
+    #Getting the data
+    major = Major.query.filter_by(id=major_id).first()
     user = Users.query.filter_by(username=username).first()
     if not major:
         return (403, "Major does not exist")
@@ -11,6 +23,7 @@ def enrol_to_major(major_name: str, username: str) -> tuple[int, str]:
         return (403, "Username does not exist")
 
     try:
+        #If it's already in there just retuning it, and if not adding it.
         if major in user.majors:
             return (202, f"{user.username} is already in {major.name}")
         user.majors.append(major)
@@ -20,14 +33,28 @@ def enrol_to_major(major_name: str, username: str) -> tuple[int, str]:
     except Exception as e:
         return (500, str(e))
 
-def enrol_to_minor(minor_name: str, username: str) -> tuple[int, str]:
-    minor = Minor.query.filter_by(name=minor_name).first()
+def enrol_to_minor(minor_id: str, username: str) -> tuple[int, str]:
+    """Function to enrol a student in a minor.
+
+    Requires:
+        - minor_id (str):
+            The id of the minor.
+        - username (str):
+            The username of the student to enrol.
+    
+    Returns:
+        - The status code and the message.
+
+    """
+    #Getting the data
+    minor = Minor.query.filter_by(id=minor_id).first()
     user = Users.query.filter_by(username=username).first()
     if not minor:
         return (403, "Minor does not exist")
     if not user:
         return (403, "Username does not exist")
     
+    #Adding to database plus a few error checking.
     try:
         if minor in user.majors:
             return (403, "Can't choose your major as your minor.")
@@ -40,14 +67,28 @@ def enrol_to_minor(minor_name: str, username: str) -> tuple[int, str]:
     except Exception as e:
         return (500, str(e))
 
-def enrol_to_spec(spec_name: str, username: str) -> tuple[int, str]:
-    spec = Specialization.query.filter_by(name=spec_name).first()
+def enrol_to_spec(spec_id: str, username: str) -> tuple[int, str]:
+    """Function to enrol a student in a specialization.
+
+    Requires:
+        - spec_id (str):
+            The id of the specialization.
+        - username (str):
+            The username of the person we want to add.
+    
+    Returns:
+        - The status + the message
+
+    """
+    #Getting the data
+    spec = Specialization.query.filter_by(id=spec_id).first()
     user = Users.query.filter_by(username=username).first()
     if not spec:
         return (403, "Specializations does not exist")
     if not user:
         return (403, "Username does not exist")
     
+    #Adding the data to database
     try:
         if spec in user.specialization:
             return (202, f"{user.username} is already in {spec.name}")
@@ -58,17 +99,23 @@ def enrol_to_spec(spec_name: str, username: str) -> tuple[int, str]:
     except Exception as e:
         return (500, str(e))
 
-def enrol_to_seq(seq_name: str, seq_plan:str, username:str):
-    print(seq_name)
-    seq_plan = seq_plan.replace("-",",")
-    seq_plan = seq_plan.replace("Coop", "Co-op")
-    seq_name = seq_name[0].lower() + seq_name[1:]
-    print(seq_plan)
-    print(seq_name)
-    sequence = Sequence.query.filter_by(name=seq_name).first()
+def enrol_to_seq(seq_id: str, username:str) -> tuple[int, str]:
+    """Function to enrol a student in a specialization.
+
+    Requires:
+        - seq_name (str):
+            The name of the sequence, prone to change.
+    Returns:
+        the status and the message.
+    """
+    sequence = Sequence.query.filter_by(id=seq_id).first()
+    user = Users.query.filter_by(username=username).first()
     if not sequence:
-        print("couldn't find")
         return 401,"selected sequence does not exist"
-    print(f"Sequence found!: name={sequence.name}")
-    print(sequence.plan)
-    print(seq_plan)
+    try:
+        user.sequence = sequence
+        db.session.add(user)
+        db.session.commit()
+        return 200,"Sequence Updated"
+    except Exception as e:
+        return 500,str(e)

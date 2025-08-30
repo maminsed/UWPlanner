4c79dee561d65426a7e55e034a0c77fd747b1bd0
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-08-30 19:39:53 -0400

    added func to connect with graphql

---
+++ frontend/src/lib/useGQL.tsx
@@ -0,0 +1,25 @@
+
+export default function useGQL() {
+    
+    return async (query: string, variables: Record<string, any> = {}) => {
+        const res = await fetch(
+            `${process.env.NEXT_PUBLIC_GQL_URL}`,
+            {
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({query, variables})
+            });
+        
+        if (!res.ok) throw new Error(`HTTP errro: ${res.status}`);
+
+        const response = await res.json().catch(()=>{})
+
+        if (response?.errors?.length) {
+            const msg = response.errors.map((i:any)=>i.message).join("; ");
+            throw new Error(`Error in GQL: ${msg}`);
+        }
+        return response
+    }
+}

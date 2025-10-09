bd7bbd4838184da8da13a5dbcd0803a332d2ca3c
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-10-08 21:56:39 -0400

    added Control and Expand Panel

--- frontend/src/app/(main)/graph/page.tsx
+++ frontend/src/app/(main)/graph/page.tsx
@@ -1,10 +1,46 @@
 import Graph from "@/components/graph/Graph";
 import PanZoomCanvas from "@/components/utils/PanZoomCanvas";
+import ExpandPanel from '@/components/utils/ExpandPanel'
+import { IoSwapHorizontalOutline } from "react-icons/io5";
+import { LuCheckCheck, LuImport, LuMinus, LuPlus, LuShare } from "react-icons/lu";
+
+function ControlPanel() {
+    return (
+        <div className="text-light-green px-3 py-2 flex flex-col items-start text-sm gap-1">
+            <button className="cursor-pointer"><LuPlus className="inline-block" /> Add a course</button>
+            <button className="cursor-pointer"><LuImport className="inline-block mr-0.2" /> Import a Semester</button>
+            <button className="cursor-pointer"><IoSwapHorizontalOutline className="inline-block" /> Reorder semesters</button>
+            <button className="cursor-pointer"><LuCheckCheck className="inline-block mr-1" />Checklists</button>
+            <button className="cursor-pointer"><LuMinus className="inline-block" /> Remove Prereqs</button>
+            <button className="cursor-pointer"><LuShare className="inline-block mr-1" />Export</button>
+        </div>
+    )
+}
+
+function ClassPanel() {
+    return (
+        <div className="">
+            Hi
+        </div>
+    )
+}
 
 export default function GraphPage() {
     return (
-        <PanZoomCanvas>
-            <Graph />
-        </PanZoomCanvas>
+        <section>
+            <PanZoomCanvas>
+                <Graph />
+            </PanZoomCanvas>
+            <div className="fixed left-6 bottom-5">
+                <ExpandPanel>
+                    <ControlPanel />
+                </ExpandPanel>
+            </div>
+            <div className="fixed right-6 bottom-5">
+                <ExpandPanel>
+                    <ClassPanel />
+                </ExpandPanel>
+            </div>
+        </section>
     );
 }
--- frontend/src/components/graph/Graph.tsx
+++ frontend/src/components/graph/Graph.tsx
@@ -3,7 +3,9 @@
 function Semester() {
     return (
         <div className="flex flex-col text-xl gap-6 items-center">
-            <div className="px-6 py-2 rounded-xl bg-white shadow-xs mb-3 w-full font-semibold text-center text-2xl whitespace-nowrap">1B - Winter</div>
+            <div className="px-6 py-2 rounded-3xl bg-white shadow-xs mb-3 w-full font-semibold text-center text-lg whitespace-nowrap">
+                WT1 - Winter 2024
+            </div>
             {[...Array(Math.floor(Math.random()*4+4))].map((_,i)=>(
                 <div key={i} className="rounded-r-xl bg-[#8AD5DF]/60 text-dark-green flex items-center">
                     <div className="bg-dark-green h-full w-2"/><span className="py-4 pr-6 pl-4">CS12{i}</span><div className="mr-2 border-1 rounded-full h-1.5 aspect-square"/>
@@ -13,10 +15,9 @@ function Semester() {
     )
 }
 
-
 export default function Graph() {
     return (
-        <div className="border-2 border-pink-800 flex gap-20 p-8">
+        <div className="flex gap-6 p-8">
             {[...Array(15)].map((_,i)=>(
                 <Semester key={i} />
             ))}

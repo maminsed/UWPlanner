e5aca95468b96d91ea331cd95efec027a95cc5b7
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2025-11-18 00:12:24 -0500

    fixed hoverEffect

--- frontend/src/components/HoverEffect.tsx
+++ frontend/src/components/HoverEffect.tsx
@@ -18,11 +18,11 @@ export default function HoverEffect({
   ...props
 }: HoverEffectProps) {
   return (
-    <div className={clsx('group relative w-fit ', outerClass)} {...props}>
+    <div className={clsx('group relative w-fit group/hovereffect', outerClass)} {...props}>
       <div className={clsx('truncate w-fit ', pClass)}>{children}</div>
       <div
         className={
-          'absolute max-h-0 py-0 overflow-x-auto text-center group-hover:max-h-15 transition-all duration-600 bg-slate-900/70 overflow-y-hidden text-green-100 text-xs px-2 group-hover:py-1 rounded-md top-5 -right-2 z-20 '
+          'absolute max-h-0 py-0 overflow-x-auto text-center group-hover/hovereffect:max-h-15 transition-all duration-600 bg-slate-900/70 overflow-y-hidden text-green-100 text-xs px-2 group-hover/hovereffect:py-1 rounded-md top-5 -right-2 z-20 '
         }
         style={hoverStyle}
       >

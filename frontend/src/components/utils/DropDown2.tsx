24d746ad5e4f30cb93687133642f87abf068e92c
Mohammadamin Sedaghat <m.aminsedaghat84@gmail.com>
2026-01-05 23:40:32 -0500

    updated DropDown to allow for groupings

--- frontend/src/components/utils/DropDown2.tsx
+++ frontend/src/components/utils/DropDown2.tsx
@@ -1,8 +1,8 @@
 'use client';
 import clsx from 'clsx';
-import { useRef, useState } from 'react';
+import { Fragment, useRef, useState } from 'react';
 
-interface DropDown2Props<T> {
+interface GroupedDropDownProps<T> {
   updateInputFunction: (value: string) => void;
   updateSelectFunction: (value: T) => void;
   valueFunction: (value: T) => string;
@@ -10,9 +10,16 @@ interface DropDown2Props<T> {
   placeholder?: string;
   options: T[];
   className?: string;
+  // If it is grouped: Make sure they are ordered.
+  // e.g. first they are all group1, then they are all group2
+  grouped?: boolean;
+  getGroup?: (value: T) => string;
+  // TODO:
+  // hover?:boolean;
+  // getHover: (value:T) => string;
 }
 
-export default function DropDown2<T>({
+export default function GroupedDropDown<T>({
   updateInputFunction,
   updateSelectFunction,
   valueFunction,
@@ -20,26 +27,28 @@ export default function DropDown2<T>({
   options,
   placeholder = '',
   className,
-}: DropDown2Props<T>) {
+  grouped = false,
+  getGroup = () => '',
+}: GroupedDropDownProps<T>) {
   const [close, setClose] = useState<boolean>(true);
   const ref = useRef<HTMLDivElement>(null);
-  const [highlitedIndex, setHighlitedIndex] = useState<number>(0);
+  const [highlightedIndex, setHighlightedIndex] = useState<number>(0);
 
   function handleKeyDown(e: React.KeyboardEvent) {
     const size = options.length;
-    if (size == 0) return;
-    let indexValue = highlitedIndex;
+    if (size === 0) return;
+    let indexValue = highlightedIndex;
     if (e.key === 'ArrowDown') {
       e.preventDefault();
-      setHighlitedIndex((indexValue + 1) % options.length);
+      setHighlightedIndex((indexValue + 1) % options.length);
     } else if (e.key === 'ArrowUp') {
       e.preventDefault();
       if (indexValue <= 0) indexValue = options.length;
-      setHighlitedIndex(indexValue - 1);
+      setHighlightedIndex(indexValue - 1);
     }
     if (e.key === 'Enter') {
       e.preventDefault();
-      if (indexValue != -1) {
+      if (indexValue !== -1) {
         updateSelectFunction(options[indexValue]);
         setClose(true);
       }
@@ -47,7 +56,7 @@ export default function DropDown2<T>({
       setClose(false);
     }
   }
-
+  let currGroup = '';
   return (
     <div
       ref={ref}
@@ -59,12 +68,12 @@ export default function DropDown2<T>({
         }
       }}
     >
-      <div className="relative">
+      <div className="relative max-w-[90vw] w-75">
         <input
-          className="border-1 rounded-sm  block w-full py-2 pl-1 pr-2 focus:outline-none focus:shadow-2xs focus:shadow-dark-green duration-75"
+          className="border-1 rounded-sm block w-full py-2 pl-1 pr-2 focus:outline-none focus:shadow-2xs focus:shadow-dark-green duration-75"
           value={currentValue ? valueFunction(currentValue) : ''}
           onChange={(e) => {
-            setHighlitedIndex(-1);
+            setHighlightedIndex(-1);
             updateInputFunction(e.currentTarget.value);
           }}
           onKeyDown={(e) => handleKeyDown(e)}
@@ -72,28 +81,42 @@ export default function DropDown2<T>({
         />
       </div>
       {!close && (
-        <div className="bg-teal-800 text-light-green py-1 rounded-sm mt-1 w-[90vw] max-w-75 scroller max-h-30 overflow-x-clip overflow-y-auto">
-          {options.map((o, k) => (
-            <div
-              className={clsx(
-                'cursor-pointer w-[92vw] max-w-75 hover:bg-teal-700 truncate px-1',
-                highlitedIndex === k && 'bg-teal-600',
-              )}
-              onClick={() => {
-                updateSelectFunction(o);
-                setClose(true);
-              }}
-              key={k}
-              ref={(el) => {
-                if (highlitedIndex === k && el) {
-                  el.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
-                }
-              }}
-            >
-              {valueFunction(o)}
-            </div>
-          ))}
-          {options.length == 0 && (
+        <div className="bg-teal-800 text-light-green py-1 rounded-sm mt-1 scroller max-h-30 overflow-x-clip overflow-y-auto max-w-[90vw] w-75">
+          {options.map((o, k) => {
+            const optionGroup = getGroup(o);
+            const group =
+              grouped && optionGroup !== currGroup ? (
+                <div className="font-medium text-lime-300/90 truncate px-1 max-w-[90vw] w-75">
+                  {optionGroup}
+                </div>
+              ) : (
+                <></>
+              );
+            currGroup = optionGroup;
+            return (
+              <Fragment key={`${getGroup(o)}-${valueFunction(o)}`}>
+                {group}
+                <div
+                  className={clsx(
+                    'cursor-pointer hover:bg-teal-700 truncate px-1 max-w-[90vw] w-75',
+                    highlightedIndex === k && 'bg-teal-600',
+                  )}
+                  onClick={() => {
+                    updateSelectFunction(o);
+                    setClose(true);
+                  }}
+                  ref={(el) => {
+                    if (highlightedIndex === k && el) {
+                      el.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+                    }
+                  }}
+                >
+                  {valueFunction(o)}
+                </div>
+              </Fragment>
+            );
+          })}
+          {options.length === 0 && (
             <div className="cursor-pointer hover:bg-teal-700 truncate px-1 "> No results </div>
           )}
         </div>
